1f254a5a133384c4af8e37a6119739eb14b931e1
fx1123 <986465231@qq.com>
2020-02-21 17:10:18 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//导入vue
+import Vue from 'vue'
+
+//按需导入Mint-UI组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+//导入MUI的样式
+import './lib/mui/css/mui.min.css'
+
+//导入app组件
+import app from './App.vue'
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
